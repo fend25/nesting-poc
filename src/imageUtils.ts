b2781cd06d5e55cc70360bf3f65b572e4855d823
Yuriy Peshekhonov <yupeshekhonov@usetech.com>
2023-02-09 11:37:39 +0300

    Update imageUtils.ts

--- src/imageUtils.ts
+++ src/imageUtils.ts
@@ -1,11 +1,13 @@
 import mergeImg from 'merge-img'
 
-export const mergeImages = (imgArray: string[], offset: number, filePath: string): void => {
+export const mergeImages = (imgArray: string[], offset: number, outputFilePath: string): void => {
   mergeImg(imgArray, {
     align: 'center',
     offset,
   }).then((img) => {
     // Save image as file
-    img.write(filePath, () => console.log(`Images were merged. The output is ${filePath}`))
+    img.write(outputFilePath, () =>
+      console.log(`Images were merged. The output is ${outputFilePath}`)
+    )
   })
 }
