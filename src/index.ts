e090f13fa3032bbba7c638e0eaed4b30bd2d9330
Yuriy Peshekhonov <yupeshekhonov@usetech.com>
2023-02-09 11:29:47 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,16 @@
 import {getSdk, getConfig} from './utils'
 import {mergeImages} from './imageUtils'
-import {Client} from '@unique-nft/sdk'
+import {Client, TokenIdQuery} from '@unique-nft/sdk'
 
-const getTokenImageUrls = async (sdk: Client, parentCollectionId: string | number): Promise<string[]> => {
+const getTokenImageUrls = async (sdk: Client, parentToken: TokenIdQuery): Promise<string[]> => {
   const imgArray: string[] = []
 
-  const token = await sdk.tokens.get({collectionId: parentCollectionId, tokenId: 1})
-  if (token.image.fullUrl) 
+  const token = await sdk.tokens.get(parentToken)
+  if (token.image.fullUrl) {
     imgArray.push(token.image.fullUrl)
+  }
 
-  const bundle = await sdk.tokens.getBundle({collectionId: parentCollectionId, tokenId: 1})
+  const bundle = await sdk.tokens.getBundle(parentToken)
   bundle.nestingChildTokens.forEach((token) => {
     imgArray.push(token.image.fullUrl)
   })
@@ -21,7 +22,10 @@ async function main() {
   const config = getConfig()
   const sdk = await getSdk(config.BASE_URL as string, config.MNEMONIC)
 
-  const imgArray = await getTokenImageUrls(sdk, config.PARENT_COLLECTION as string)
+  const imgArray = await getTokenImageUrls(sdk, {
+    collectionId: config.PARENT_COLLECTION as string,
+    tokenId: 1,
+  })
   mergeImages(imgArray, Number(config.OFFSET), config.FILE_PATH as string)
 }
 
