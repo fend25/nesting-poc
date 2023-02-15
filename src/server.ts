dce2eb11330326fef483aec13d59af80216757a5
Yuriy Peshekhonov <yupeshekhonov@usetech.com>
2023-02-15 12:51:28 +0300

    Router added
    image distribution works as before

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,18 @@
 import * as fs from 'node:fs'
 import {getConfig, getSdk} from './utils'
 //@ts-ignore
+import Router from '@koa/router'
 import Koa from 'koa'
 import {getTokenImageUrls} from '.'
 import {mergeImages} from './imageUtils'
 
+let lastRenderTime = 0
+const CACHE_TIME = 10 * 1000
+
 const config = getConfig()
 
 const app = new Koa()
-
-let lastRenderTime = 0
-const CACHE_TIME = 10 * 1000
+const router = new Router()
 
 const rerenderAndSaveImage = async () => {
   const sdk = await getSdk(config.baseUrl, config.mnemonic)
@@ -23,10 +25,8 @@ const rerenderAndSaveImage = async () => {
   const file = await mergeImages(imgArray, config.offset, `${config.imagesDir}/${config.fileName}`)
 }
 
-app.use(async (ctx) => {
-  console.log('Working ...')
-
-  if (ctx.request.url === `/image/${config.parentCollection}/${config.parentToken}`) {
+router
+  .get(`/image/${config.parentCollection}/${config.parentToken}`, async (ctx) => {
     const path = `${config.imagesDir}/${config.fileName}`
     if (Date.now() - lastRenderTime > CACHE_TIME) {
       await rerenderAndSaveImage()
@@ -35,11 +35,20 @@ app.use(async (ctx) => {
     const stream = fs.createReadStream(path)
     ctx.response.set('content-type', 'image/png')
     ctx.body = stream
-  } else {
+  })
+  .get('/', (ctx) => {
     ctx.body =
       'Server is running. The image is available at <server>:<port>/image/<parent-collection-id>/<parent-token-id>'
-  }
-})
+  })
+
+/* app.use(async (ctx) => {
+  console.log('Working ...')
+
+  if (ctx.request.url === `/image/${config.parentCollection}/${config.parentToken}`) {
+    
+}) */
+
+app.use(router.routes()).use(router.allowedMethods())
 
 app.listen(config.port, config.host, () => {
   console.log(`Server listening on ${config.host}:${config.port}`)
